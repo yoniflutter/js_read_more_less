f0cf6d6762c1bf41d20b9d4a9f588caf380f5c96
yoniflutter <yohannessintayehu1@gmail.com>
2023-09-06 20:26:36 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const p = document.querySelectorAll('p');
+
+p.forEach(elem =>{
+    const text = elem.textContent;
+    const span = document.createElement('span');
+    span.textContent = text;
+    if(elem.textContent.length > 25){
+        var isMore = true;
+        var isSliced = true;
+        elem.textContent = "";
+        const sliced = span.textContent.slice(0, 25);
+        span.textContent = `${sliced}...` ;
+        const a = document.createElement('a');
+        a.textContent = "Read more";
+        a.addEventListener('click', ()=>{
+            isSliced = !isSliced;
+            span.textContent = (isSliced) ? sliced : text;
+            isMore = !isMore;
+            a.textContent = (isMore) ? "Read more" : "Read less";
+        })
+        elem.append(span, a);
+    }
+})
